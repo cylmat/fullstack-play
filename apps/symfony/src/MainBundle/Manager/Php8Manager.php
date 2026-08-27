40b0c8c5edd1157356c67c9100af0a7e293b4ebc
cylmat <cyrilmatte.pro@gmail.com>
2026-08-27 13:47:42 +0000

    Update by make

--- apps/symfony/src/MainBundle/Manager/Php8Manager.php
+++ apps/symfony/src/MainBundle/Manager/Php8Manager.php
@@ -2,22 +2,50 @@
 
 namespace App\MainBundle\Manager;
 
+use Attribute;
+
+#[Attribute]
+class SetUpAttributeUsage { public function __construct(public int $value) {} }
+
+class Testing {
+    #[SetUpAttributeUsage(6)]
+    public function usingTest(): void {}
+}
+
 class Php8Manager
 {
+    public function __construct(
+        private string $constructorPromotion80 = 'PHP 8.0',
+    ) { }
+
     /** @return array<string, mixed> */
-    public function php80(): array
-    {
+    public function php80(
+        int|bool $typeUnion = 1,
+    ): array {
         // @see https://www.php.net/manual/en/migration80.new-features.php
-        $namedArguments = array_fill(start_index: 0, count: 5, value: 'array_fill(start_index: 0);');
+        $namedArguments = array_fill(start_index: 0, count: 5, value: 1);
+        $reflectionAttribute = new \ReflectionClass(Testing::class);
+        $methodWithAttribute = $reflectionAttribute->getMethod('usingTest')->getAttributes(SetUpAttributeUsage::class)[0];
+        $matching = match ('one') { 'one' => "Displaying", 'two' => "Error", };
+        $nullSafe = $reflectionAttribute?->getConstant('NON_EXISTENT_CONSTANT');
+        $stringCompare = 0 == 'foobar'; // false
+        // $strlenType = strlen([]);
 
         return [
-            'Union Types' => null,
-            'NamedArguments' => $namedArguments[0],
-            'Attributes' => null,
-            'Constructor Property Promotion' => null,
-            'Match Expression' => null,
-            'Nullsafe Operator' => null,
-            'JIT' => null,
+            'title' => 'PHP 8.0',
+            'ref' => 'https://www.php.net/releases/8.0',
+
+            'NamedArguments' => 'array_fill(0, count: 5)',
+            'Attributes' => '#[Attribute] class SetUpAttributeUsage {}',
+            'Constructor Property Promotion' => 'function __construct(public float $x = 0.0)',
+            'Union Types' => 'private int|float $number',
+            'Match Expression' => "return match ('one') {'one' => \"Displaying\",}",
+            'Nullsafe Operator' => '$country = $session?->user?->getAddress()?->country;',
+            'Saner string to number comparisons' => "0 == 'foobar' // false",
+            'Consistent type errors' => 'strlen([]); // TypeErrorException',
+            'JIT compilation' => null,
+            'Improve' => 'mixed, return static',
+            'NEW' => 'Weak Map, StringableInterface, str_contains(), str_starts_with(), str_ends_with(), fdiv()',
         ];
     }
 }
