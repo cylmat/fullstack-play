576582acf3cd4792301883c9f502b158217d72b5
cylmat <cyrilmatte.pro@gmail.com>
2026-09-13 13:10:22 +0000

    ai exemple

--- scripts/git.sh
+++ scripts/git.sh
@@ -8,6 +8,7 @@ if [ "$1" == "ls" ] || [ "$1" == "" ] || [ "$1" == "help" ]; then
     echo ""
     echo 'Listing available "Run Script" git commands:'
     echo "push"
+    exit 0
 fi
 
 
@@ -24,8 +25,11 @@ if [ "$1" == "push" ]; then
     docker run --rm -u 1000:1000 --env-file .docker/linux/.env.local \
         -v .:/var/www/application -v .docker/data/linux:/data fs-linux \
         sh -c "$CMD"
+    exit 0
 fi
 
+echo "Error: Run Git '$1' not found!"
+exit 1
 
 # getopts "m:" opt
 # while getopts "m:" opt; do
