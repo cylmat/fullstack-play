f57023de7236a56305c562e4f370c936b6c266ca
cylmat <cyrilmatte.pro@gmail.com>
2026-09-14 20:30:05 +0000

    change ai preact init

--- ai/vite.config.ts
+++ ai/vite.config.ts
@@ -1,7 +1,17 @@
-import preact from '@preact/preset-vite'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+	plugins: [
+		preact({
+			prerender: {
+				enabled: true,
+				renderTarget: '#app',
+				additionalPrerenderRoutes: ['/404'],
+				previewMiddlewareEnabled: true,
+				previewMiddlewareFallback: '/404',
+			},
+		}),
+	],
+});
